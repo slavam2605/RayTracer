516e3927834df99d22d95b19d30f6f94eb862a25
slava <slavam2605@bk.ru>
2016-03-29 00:57:53 +0300

    lel

--- main.cpp
+++ main.cpp
@@ -1,18 +1,165 @@
+#include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <cmath>
 #include <vector>
+#include <ctime>
+#include <memory>
 
 using namespace std;
 
 struct color {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
+    float r;
+    float g;
+    float b;
 
-    color(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
-    color(): r(0), g(0), b(0) {}
+    color(float r, float g, float b) : r(r), g(g), b(b) { }
+
+    color() : r(0), g(0), b(0) { }
+
+    color operator*(const color& o) const {
+        return color(r * o.r, g * o.g, b * o.b);
+    }
+
+    color& operator+=(const color& o) {
+        r += o.r;
+        g += o.g;
+        b += o.b;
+        return *this;
+    }
+
+    color& operator/=(float a) {
+        r /= a;
+        g /= a;
+        b /= a;
+        return *this;
+    }
+
 };
+
+const float PI = 3.14159265358979323f;
+
+struct hsv;
+
+inline hsv hsv2hsl(const hsv &cl);
+
+inline hsv hsl2hsv(const hsv &cl);
+
+struct hsv {
+    float h;
+    float s;
+    float v;
+
+
+    hsv(float h, float s, float v) : h(h), s(s), v(v) { }
+
+
+    hsv() { }
+
+    hsv mix(const hsv &o) {
+        hsv hsl1 = hsv2hsl(*this);
+        hsv hsl2 = hsv2hsl(o);
+        float x1 = hsl1.s * cos(PI * hsl1.h / 180);
+        float y1 = hsl1.s * sin(PI * hsl1.h / 180);
+        float z1 = hsl1.v;
+        float x2 = hsl2.s * cos(PI * hsl2.h / 180);
+        float y2 = hsl2.s * sin(PI * hsl2.h / 180);
+        float z2 = hsl2.v;
+        x1 = (x1 + x2) / 2;
+        y1 = (y1 + y2) / 2;
+        z1 = (z1 + z2) / 2;
+        return hsl2hsv(hsv((atan2f(y1, x1) + PI) * 180 / PI, sqrt(x1 * x1 + y1 * y1), z1));
+    }
+};
+
+inline hsv hsv2hsl(const hsv &cl) {
+    float hue2 = (2 - cl.s) * cl.v;
+    return hsv(
+            cl.h,
+            cl.s * cl.v / (hue2 < 1 ? hue2 : 2 - hue2),
+            hue2 / 2
+    );
+}
+
+inline hsv hsl2hsv(const hsv &cl) {
+    float sat = cl.s * (cl.v < 0.5 ? cl.v : 1 - cl.v);
+    return hsv(
+            cl.h,
+            2 * sat / (cl.v + sat),
+            cl.v + sat
+    );
+}
+
+hsv rgb2hsv(color in) {
+    hsv out;
+    float min, max, delta;
+
+    min = in.r < in.g ? in.r : in.g;
+    min = min < in.b ? min : in.b;
+
+    max = in.r > in.g ? in.r : in.g;
+    max = max > in.b ? max : in.b;
+
+    out.v = max;                                // v
+    delta = max - min;
+    if (delta < 0.00001f) {
+        out.s = 0;
+        out.h = 0; // undefined, maybe nan?
+        return out;
+    }
+    if (max > 0.0f) { // NOTE: if Max is == 0, this divide would cause a crash
+        out.s = (delta / max);                  // s
+    } else {
+        // if max is 0, then r = g = b = 0
+        // s = 0, v is undefined
+        out.s = 0.0f;
+        out.h = NAN;                            // its now undefined
+        return out;
+    }
+    if (in.r >= max)                           // > is bogus, just keeps compilor happy
+        out.h = (in.g - in.b) / delta;        // between yellow & magenta
+    else if (in.g >= max)
+        out.h = 2.0f + (in.b - in.r) / delta;  // between cyan & yellow
+    else
+        out.h = 4.0f + (in.r - in.g) / delta;  // between magenta & cyan
+
+    out.h *= 60.0f;                              // degrees
+
+    if (out.h < 0.0f)
+        out.h += 360.0f;
+
+    return out;
+}
+
+
+color hsv2rgb(hsv in) {
+    if (in.s <= 0.0) {
+        return color(in.v, in.v, in.v);
+    }
+    float hh = in.h / 60.0f;
+    if (hh >= 6) hh = 0.0f;
+    long i = hh;
+    float ff = hh - i;
+    float p = in.v * (1.0f - in.s);
+    float q = in.v * (1.0f - in.s * ff);
+    float t = in.v * (1.0f - in.s * (1.0f - ff));
+
+    switch (i) {
+        case 0:
+            return color(in.v, t, p);
+        case 1:
+            return color(q, in.v, p);
+        case 2:
+            return color(p, in.v, t);
+        case 3:
+            return color(p, q, in.v);
+        case 4:
+            return color(t, p, in.v);
+        case 5:
+        default:
+            return color(in.v, p, q);
+    }
+}
 
 struct vec3f {
     float x;
@@ -20,13 +167,18 @@ struct vec3f {
     float z;
 
     vec3f(float x, float y, float z) : x(x), y(y), z(z) { }
-    vec3f(): x(0), y(0), z(0) {}
 
-    vec3f operator-(const vec3f& b) const {
+    vec3f() : x(0), y(0), z(0) { }
+
+    vec3f operator+(const vec3f &b) const {
+        return vec3f(this->x + b.x, this->y + b.y, this->z + b.z);
+    }
+
+    vec3f operator-(const vec3f &b) const {
         return vec3f(this->x - b.x, this->y - b.y, this->z - b.z);
     }
 
-    float operator*(const vec3f& b) const {
+    float operator*(const vec3f &b) const {
         return this->x * b.x + this->y * b.y + this->z * b.z;
     }
 
@@ -34,10 +186,21 @@ struct vec3f {
         return vec3f(a * this->x, a * this->y, a * this->z);
     }
 
-    vec3f& operator/=(float a) {
+    vec3f operator/(float a) const {
+        return vec3f(this->x / a, this->y / a, this->z / a);
+    }
+
+    vec3f &operator/=(float a) {
         this->x /= a;
         this->y /= a;
         this->z /= a;
+        return *this;
+    }
+
+    vec3f &operator*=(float a) {
+        this->x *= a;
+        this->y *= a;
+        this->z *= a;
         return *this;
     }
 
@@ -47,21 +210,12 @@ struct ray {
     vec3f r0;
     vec3f s;
 
-    ray(const vec3f &r0, const vec3f &s) : r0(r0), s(s) {}
+    ray(const vec3f &r0, const vec3f &s) : r0(r0), s(s) { }
+
     ray(float x0, float y0, float z0, float sx, float sy, float sz)
-            : r0(x0, y0, z0), s(sx, sy, sz) {}
-    ray() {}
-};
+            : r0(x0, y0, z0), s(sx, sy, sz) { }
 
-struct sphere {
-    vec3f c;
-    float R;
-    color cl;
-
-    sphere(const vec3f &c, float R, color cl = color(255, 255, 255)) : c(c), R(R), cl(cl) { }
-    sphere(float x, float y, float z, float R, color cl = color(255, 255, 255))
-            : c(x, y, z), R(R), cl(cl) {}
-    sphere() {}
+    ray() { }
 };
 
 const float INF = 1e10;
@@ -70,46 +224,145 @@ float sqr(float x) {
     return x * x;
 }
 
-float intersect(const ray& r, const sphere& sp) {
-    vec3f dr = sp.c - r.r0;
-    float D = sqr(r.s * dr) + sqr(sp.R) - dr * dr;
-    if (D < 0) return 2 * INF;
-    return r.s * dr - sqrt(D);
+struct object {
+    virtual float intersect(const ray &r) = 0;
+
+    virtual color get_color(const ray &r, const vec3f &p, int depth) = 0;
+};
+
+vec3f view(256 / 2, 256 / 2, -256);
+vector<unique_ptr<object>> scene;
+vector<unique_ptr<object>> light;
+
+vec3f rand_dir() {
+    while (true) {
+        int x = 2 * rand() - RAND_MAX;
+        int y = 2 * rand() - RAND_MAX;
+        int z = 2 * rand() - RAND_MAX;
+        int len = x * x + y * y + z * z;
+        if (len <= RAND_MAX * RAND_MAX) {
+            float sq_len = sqrtf(len);
+            return vec3f(x / sq_len, y / sq_len, z / sq_len);
+        }
+    }
 }
 
-vec3f view(256, 256, -256);
-vector<sphere> spheres;
+const int DEPTH_LIMIT = 1;
 
-color get_pixel(int x, int y) {
+color get_color(const ray &r, int depth);
+
+struct sphere : object {
+    vec3f c;
+    float R;
+    color cl;
+    bool is_light;
+
+    sphere(const vec3f &c, float R, color cl = color(1, 1, 1), bool is_light = false) : c(c), R(R), cl(cl), is_light(is_light) { }
+
+    sphere(float x, float y, float z, float R, color cl = color(1, 1, 1), bool is_light = false)
+            : c(x, y, z), R(R), cl(cl), is_light(is_light) { }
+
+    sphere() { }
+
+    float intersect(const ray &r) {
+        vec3f dr = c - r.r0;
+        float D = sqr(r.s * dr) + sqr(R) - dr * dr;
+        if (D < 0) return 2 * INF;
+        return r.s * dr - sqrt(D);
+    }
+
+    color get_color(const ray &r, const vec3f &p, int depth) {
+        if (is_light) return cl;
+        if (depth > DEPTH_LIMIT) return color(0, 0, 0);
+        color light_c;
+        vec3f norm = (p - c) / R;
+        int n = 1000;
+        for (int i = 0; i < n; i++) {
+            vec3f dir = rand_dir();
+            if (dir * norm < 0) dir *= -1;
+            light_c += ::get_color(ray(p, dir), depth + 1);
+        }
+        light_c /= n;
+        return cl * light_c;
+    }
+};
+
+ofstream &print(ofstream &fout, const color &pixel);
+
+color get_color(float x, float y) {
     vec3f s = vec3f(x, y, 0) - view;
     s /= sqrt(s * s);
     ray r(view, s);
+    return get_color(r, 1);
+}
+
+color get_color(const ray &r, int depth) {
     int min_id = 0;
-    float min_dist = intersect(r, spheres[0]);
-    for (int i = 1; i < spheres.size(); i++) {
-        float dist = intersect(r, spheres[0]);
+    float min_dist = scene[0]->intersect(r);
+    for (int i = 1; i < scene.size(); i++) {
+        float dist = scene[i]->intersect(r);
         if (dist < min_dist) {
             min_id = i;
             min_dist = dist;
         }
     }
     if (min_dist > INF) return color(0, 0, 0);
-    return spheres[min_id].cl;
+    return scene[min_id]->get_color(r, r.s * min_dist + r.r0, depth);
+}
+
+
+color mix2(color c1, color c2) {
+    return hsv2rgb(rgb2hsv(c1).mix(rgb2hsv(c2)));
+}
+
+color get_pixel(int x, int y) {
+    return mix2(
+            mix2(get_color(x - 0.25f, y - 0.25f),
+                 get_color(x - 0.25f, y + 0.25f)),
+            mix2(get_color(x + 0.25f, y - 0.25f),
+                 get_color(x + 0.25f, y + 0.25f)));
 }
 
 int main() {
-    spheres.push_back(sphere(256, 256, 512, 256, color(255, 0, 0)));
+    int w = 256;
+    int h = 256;
 
-    ofstream fout("E:\\C++ Projects\\RayTracer\\out.ppm", ios::out | ios::binary);
-    fout << "P6 512 512 255 ";
+//    scene.push_back(unique_ptr<sphere>(new sphere(w / 2 - w / 4, h / 2, w / 2, w / 2, color(1, 0, 0))));
+//    scene.push_back(unique_ptr<sphere>(new sphere(w / 2 + w / 6, h / 2, w / 2, w / 2, color(0, 1, 0))));
+//    light.push_back(unique_ptr<sphere>(new sphere(w, -h, w / 2, w / 3, color(10, 10, 10))));
+
+    scene.push_back(unique_ptr<sphere>(new sphere(w / 2, h / 2, w / 2, w / 2, color(1, 0, 0))));
+    scene.push_back(unique_ptr<sphere>(new sphere(w, 0, w, w / 3, color(10, 10, 10), true)));
+
+    view = vec3f(w / 2, h / 2, -w / 2);
+    int time = clock();
+//    string path = "E:\\C++ Projects\\RayTracer\\";
+    string path = "C:\\Users\\slava\\ClionProjects\\RayTracer\\";
+    ofstream fout(path + "out.ppm", ios::out | ios::binary);
+    fout << "P6 " << w << " " << h << " 255 ";
     color pixel;
-    for (int i = 0; i < 512; i++) {
-        for (int j = 0; j < 512; j++) {
+    for (int j = 0; j < w; j++) {
+        for (int i = 0; i < h; i++) {
             pixel = get_pixel(i, j);
-            fout << pixel.r << pixel.g << pixel.b;
+            print(fout, pixel);
         }
     }
     fout.close();
-    system("convert \"E:\\C++ Projects\\RayTracer\\out.ppm\" \"E:\\C++ Projects\\RayTracer\\out.png\"");
+    cout << float(clock() - time) / CLOCKS_PER_SEC << " seconds" << endl;
+    system(("convert \"" + path + "out.ppm\" \"" + path + "out.png\"").c_str());
     return 0;
+}
+
+ofstream &print(ofstream &fout, const color &pixel) {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    if (pixel.r < 0) r = 0; else if (pixel.r > 1) r = 255; else
+        r = (unsigned char) (pixel.r * 255);
+    if (pixel.g < 0) g = 0; else if (pixel.g > 1) g = 255; else
+        g = (unsigned char) (pixel.g * 255);
+    if (pixel.b < 0) b = 0; else if (pixel.b > 1) b = 255; else
+        b = (unsigned char) (pixel.b * 255);
+    fout << r << g << b;
+    return fout;
 }
